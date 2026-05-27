66f4fec0eab796219d198a6410aabb71f9bcc3a0
marcelinebenao634-svg <marcelinebenao634@gmail.com>
2026-05-27 05:38:28 -0800

    Update main.py

--- main.py
+++ main.py
@@ -15,4 +15,9 @@ preferences_list = preference.split(" , ")
 for pref in preferences_list:
   t.add_preference(pref.strip())
 print(t.display())
+
+post = input("Enter your post: ")
+salary = input("Enter your salary: ")
+e = Employee(name, firstname, contact, post, salary)
+print(e.display())
                    
