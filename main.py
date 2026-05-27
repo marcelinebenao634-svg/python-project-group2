eda332cf0a92e116c4f0e39b488ea7290cb9a3fb
marcelinebenao634-svg <marcelinebenao634@gmail.com>
2026-05-27 05:01:55 -0800

    Add user input for Person and Tourist classes

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+from person import Person
+from person import Tourist
+from person import Employee
 
+name = input("Enter your name :")
+firstname = input("Enter your firstname : ")
+contact = input("Enter your contact : ")
+
+p = Person(name, firstname, contact)
+print(p.display())
+
+t = Tourist(name, firstname, contact)
+preference = input("
