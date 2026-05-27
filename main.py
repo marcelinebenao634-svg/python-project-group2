c925eb98982599db9a0e19cd2f99d06c43b76120
marcelinebenao634-svg <marcelinebenao634@gmail.com>
2026-05-27 05:28:11 -0800

    Fix input handling for preferences in main.py

--- main.py
+++ main.py
@@ -10,4 +10,9 @@ p = Person(name, firstname, contact)
 print(p.display())
 
 t = Tourist(name, firstname, contact)
-preference = input("
+preference = input("Enter your preferences (separated by comma): ")
+preferences_list = preference.split(" , ")
+for pref in preferences_list:
+  t.add_preference(pref.strip())
+print(t.display())
+                   
