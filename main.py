72c8dc9ced05f372b73fb01742f6ef893c3fb12d
marcelinebenao634-svg <marcelinebenao634@gmail.com>
2026-05-27 17:29:12 -0800

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ t.add_preference(preference)
 
 while True:
   choice = input("Do you want to add more preferences? (yes/no:) ")
-  if choice =="no"
+  if choice =="no":
     break
   preference = input("Enter  your preference: ")
   t.add_preference(preference)
@@ -25,25 +25,4 @@ post = input("Enter your post: ")
 salary = input("Enter your salary: ")
 e = Employee(name, firstname, contact, post, salary)
 print(e.display())
-
-name = input("Enter hotel's name : ")
-localisation = input("Enter the localisation : ")
-cost = input("Enter the cost : ")
-stars = input("Enter the number of stars : ")
-services = input("Enter the services : ")
-services_list = []
-for service in services.split(","):
-  services_list.append(service.strip())
-cost = int(cost)
-stars = int(stars)
-h = Hotel(name, localisation, cost, stars, services_list)
-h.display()
-
-name = input("Enter auberge's name : ")
-localisation = input("Enter the localisation : ")
-cost = input("Enter the cost : ")
-type_dormitory = input("Enter the type of dormitory : ")
-cost = int(cost)
-a = Auberge(name, localisation, cost, type_dormitory)
-a.display()
 
