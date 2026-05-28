5c3ae65be330ee55790ac976d10a031f4fc1193d
marcelinebenao634-svg <marcelinebenao634@gmail.com>
2026-05-28 12:45:56 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from Person import Person Tourist, Employee
-from Logement import Hotel, Auberge
+from Logement import Hotel, Auberge,hotels, auberges
 from TouristSite import search_site, site_list
 from reservation import
 
@@ -28,7 +28,7 @@ salary = input("Enter your salary: ")
 e = Employee(name, firstname, contact, post, salary)
 print(e.display())
 
-
+# Afficher le type de logement
 print("what type of accomodation do you want?")
 print("1. Hotel")
 print("2. Auberge")
@@ -41,13 +41,13 @@ if type_choix == "1":
   h = Hotel[choice]
   h.display()
 
-  elif type_choix == "2":
+elif type_choix == "2":
   print("\nAvailable auberges: ")
   for i, hotel in enumerate(Auberge, 1):
     print(f"{i}. {Auberge.get_name()} - {Auberge.get_localisation()} - {Auberge.get_cost()} FCFA ")
   choice = int(input("Enter number: ")) - 1
   a = Auberge[choice]
-  a.display()        
+  a.display()
 
 
 
